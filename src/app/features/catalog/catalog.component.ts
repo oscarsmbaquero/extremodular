ead4d53c90cbb4c6c46d0b70bd59a227b012f4e6
oscarsmbaquero <oscarsmbaquero@gmail.com>
2026-04-27 20:50:18 +0200

    logo blanco

--- src/app/features/catalog/catalog.component.ts
+++ src/app/features/catalog/catalog.component.ts
@@ -2,13 +2,12 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { DataService } from '../../core/services/data.service';
 import { ConfiguratorService } from '../../core/services/configurator.service';
 import { Router } from '@angular/router';
-import { CurrencyPipe } from '@angular/common';
 import { TranslatePipe } from '../../core/pipes/translate.pipe';
 
 @Component({
   selector: 'app-catalog',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CurrencyPipe, TranslatePipe],
+  imports: [ TranslatePipe],
   templateUrl: './catalog.component.html',
 })
 export class CatalogComponent {
